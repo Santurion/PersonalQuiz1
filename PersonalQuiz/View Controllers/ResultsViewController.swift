e90c9d1fd49492e90b49b86de506a5afa25b7f5d
Aleksander Shuplyk <santurionley@rambler.ru>
2020-10-28 09:04:41 +0300

    2

--- PersonalQuiz/View Controllers/ResultsViewController.swift
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -10,10 +10,39 @@ import UIKit
 class ResultsViewController: UIViewController {
     
   
-
+    @IBOutlet var resultsNavigationItem: UINavigationItem!
+    @IBOutlet var animalLabel: UILabel!
+    @IBOutlet var characteristicLabel: UILabel!
+    
+    var choosenAnswers: [Answer]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setupNavigationItem()
+        setupLabels()
+    }
+    
+    private func setupNavigationItem() {
+        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = nil
+    }
+    
+    private func setupLabels() {
+        if let animalType = frequentAnimalTypeInAnswers() {
+            animalLabel.text = "Вы - \(animalType.rawValue)!"
+            characteristicLabel.text = animalType.definition
+        }
+    }
+    
+    private func frequentAnimalTypeInAnswers() -> AnimalType? {
+        var countAnimalTypes: [AnimalType: Int] = [:]
+        for answer in choosenAnswers {
+            let answerType = answer.type
+            countAnimalTypes[answerType] = (countAnimalTypes[answerType] ?? 0) + 1
+        }
+        
+        return countAnimalTypes.max { $0.value < $1.value }?.key
     }
 
 }
