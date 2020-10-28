1feedc0647c5c3c3ccd6ee12b3218a944284ffae
Aleksander Shuplyk <santurionley@rambler.ru>
2020-10-28 09:01:33 +0300

    1

--- PersonalQuiz/View Controllers/ResultsViewController.swift
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -9,10 +9,7 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты на экране
-    // 4. Избавится от кнопки возврата на предыдущий экран
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
