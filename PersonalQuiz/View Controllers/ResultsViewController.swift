9b77e357b6f63552501240b84d632389e72b1561
Aleksander Shuplyk <santurionley@rambler.ru>
2020-10-28 09:28:58 +0300

    3

--- PersonalQuiz/View Controllers/ResultsViewController.swift
+++ PersonalQuiz/View Controllers/ResultsViewController.swift
@@ -9,12 +9,11 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
-  
     @IBOutlet var resultsNavigationItem: UINavigationItem!
     @IBOutlet var animalLabel: UILabel!
     @IBOutlet var characteristicLabel: UILabel!
     
-    var choosenAnswers: [Answer]!
+    var answersChoosen: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,12 +36,11 @@ class ResultsViewController: UIViewController {
     
     private func frequentAnimalTypeInAnswers() -> AnimalType? {
         var countAnimalTypes: [AnimalType: Int] = [:]
-        for answer in choosenAnswers {
+        for answer in answersChoosen {
             let answerType = answer.type
             countAnimalTypes[answerType] = (countAnimalTypes[answerType] ?? 0) + 1
         }
         
         return countAnimalTypes.max { $0.value < $1.value }?.key
     }
-
 }
